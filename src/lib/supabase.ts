77d81b2e24905756b83d93118b0c52eedf369e40
Sibione1 <sibione1@gmail.com>
2026-07-22 22:57:56 -0300

    fix: hardcode public anon key to avoid vercel env issues

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://cgancnqrvsnxjbylluvd.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'cole_aqui_a_sua_anon_key';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_JZrKcI88vvUBBk6n8AhYVw_dXcDAT7c';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
